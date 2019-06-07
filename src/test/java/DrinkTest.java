4ca89deb65d0dd3ea00f4c852970f49b045334f5
Danny Welsh <dannywelsh400@hotmail.com>
2019-06-07 12:12:13 +0100

    Creates Drink class with passing tests

--- src/test/java/DrinkTest.java
+++ src/test/java/DrinkTest.java
@@ -1,2 +1,26 @@
+import VendingMachine.Products.Drink;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DrinkTest {
+
+        private Drink drink;
+
+        @Before
+        public void setUp() {
+            drink = new Drink("Fanta");
+        }
+
+        @Test
+        public void canGetType() {
+            assertEquals("Drink", drink.getType());
+        }
+
+        @Test
+        public void canGetName() {
+            assertEquals("Fanta", drink.getName());
+        }
+
 }
