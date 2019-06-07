f7762b115a6eb7ad29936453f3e91d825473e694
Danny Welsh <dannywelsh400@hotmail.com>
2019-06-07 12:10:26 +0100

    Creates Crisp class with passing tests

--- src/test/java/CrispTest.java
+++ src/test/java/CrispTest.java
@@ -1,2 +1,26 @@
+import VendingMachine.Products.Crisp;
+import VendingMachine.Products.Sweet;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CrispTest {
+
+        private Crisp crisp;
+
+        @Before
+        public void setUp() {
+            crisp = new Crisp("Salt & Vinegar");
+        }
+
+        @Test
+        public void canGetType() {
+            assertEquals("Crisp", crisp.getType());
+        }
+
+        @Test
+        public void canGetName() {
+            assertEquals("Salt & Vinegar", crisp.getName());
+        }
 }
