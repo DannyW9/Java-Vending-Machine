6fe316889820444f2f02905d7adf71c9bf9af733
Danny Welsh <dannywelsh400@hotmail.com>
2019-06-07 11:52:30 +0100

    Creates Coin class with basic tests passing

--- src/main/java/VendingMachine/Money/Coin.java
+++ src/main/java/VendingMachine/Money/Coin.java
@@ -1,4 +1,22 @@
 package VendingMachine.Money;
 
 public class Coin {
+
+    private CoinType coinType;
+
+    public Coin(CoinType coinType) {
+        this.coinType = coinType;
+    }
+
+    public CoinType getCoinType() {
+        return coinType;
+    }
+
+    public int getCoinValue(){
+        return getCoinType().getValue();
+    }
+
+    public boolean checkCoinValidity(){
+        return getCoinType().isValid();
+    }
 }
--- src/test/java/CoinTest.java
+++ src/test/java/CoinTest.java
@@ -1,2 +1,32 @@
+import VendingMachine.Money.Coin;
+import VendingMachine.Money.CoinType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class CoinTest {
+
+    private Coin coin1;
+
+    @Before
+    public void setUp() {
+        coin1 = new Coin(CoinType.FIFTY);
+    }
+
+    @Test
+    public void canGetCoinType() {
+        assertEquals(CoinType.FIFTY, coin1.getCoinType());
+    }
+
+    @Test
+    public void canGetValue() {
+        assertEquals(50, coin1.getCoinValue());
+    }
+
+    @Test
+    public void canGetValidity() {
+        assertTrue(coin1.checkCoinValidity());
+    }
 }
