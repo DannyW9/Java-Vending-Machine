d080ec213049ef8b7302c30b9cc74e3cc141fb02
Danny Welsh <dannywelsh400@hotmail.com>
2019-06-09 22:09:30 +0100

    Creates Credit class with passing tests for adding coins and getting the total inserted value

--- src/main/java/VendingMachine/Machine/Credit.java
+++ src/main/java/VendingMachine/Machine/Credit.java
@@ -1,4 +1,31 @@
 package VendingMachine.Machine;
+import VendingMachine.Money.Coin;
+import java.util.ArrayList;
 
 public class Credit {
+
+    private ArrayList<Coin> coins;
+
+    public Credit() {
+        this.coins = new ArrayList<Coin>();
+    }
+
+    public int getCoins() {
+        return this.coins.size();
+    }
+
+    public int totalAmount(){
+        int total = 0;
+        for (Coin coin : this.coins){
+            total += coin.getCoinValue();
+        }
+        return total;
+    }
+
+    public void addCoin(Coin coin){
+        this.coins.add(coin);
+    }
+
+
+
 }
--- src/test/java/CreditTest.java
+++ src/test/java/CreditTest.java
@@ -1,2 +1,40 @@
+import VendingMachine.Machine.Credit;
+import VendingMachine.Money.Coin;
+import VendingMachine.Money.CoinType;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class CreditTest {
+
+    private Credit credit;
+    private Coin coin1;
+    private Coin coin2;
+    private Coin coin3;
+
+
+    @Before
+    public void setUp() {
+        credit = new Credit();
+        coin1 = new Coin(CoinType.FIFTY);
+        coin2 = new Coin(CoinType.TWOHUNDRED);
+        coin3 = new Coin(CoinType.ONE);
+        credit.addCoin(coin1);
+        credit.addCoin(coin2);
+    }
+
+    @Test
+    public void canAddCoinsAndReturnCoinArrayList() {
+        assertEquals(2, credit.getCoins());
+    }
+
+    @Test
+    public void canGetTotalAmountOfCoinsCurrentlyInserted() {
+        assertEquals(250, credit.totalAmount());
+    }
+
 }
